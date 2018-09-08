8429be69076d469d173477960ce07170e904343b
Yonatan Rojas S <yonatanrojas09@gmail.com>
2018-09-08 12:23:55 -0500

    created play-pause buttons UI

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react'
 import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video';
 import Title from '../components/title';
+import PlayPause from '../components/play-pause';
 
 
 class VideoPlayer extends Component {
+
+  state = {
+    pause: true,
+  }
+
+  toggleClick = (event) => {
+    this.setState({
+      pause: !this.state.pause
+    })
+  }
+
   render() {
     return(
       <VideoPlayerLayout>
         <Title 
-          title="Este es el título del video"/>
+          title="Este es el título del video" />
+        <PlayPause 
+          pause={this.state.pause}
+          handleClick={this.toggleClick} />
         <Video
           autoplay={false}
           src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4" />
