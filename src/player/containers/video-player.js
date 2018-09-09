3715712f34f0d6f56843128240180310d89c4793
Yonatan Rojas S <yonatanrojas09@gmail.com>
2018-09-08 20:47:40 -0500

    added progress bar of video duration

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -5,6 +5,7 @@ import Title from '../components/title';
 import PlayPause from '../components/play-pause';
 import Timer from '../components/timer';
 import VideoPlayerControls from '../components/video-player-controls';
+import ProgressBar from '../components/progress-bar';
 
 
 class VideoPlayer extends Component {
@@ -40,6 +41,10 @@ class VideoPlayer extends Component {
     })
   }
 
+  handleProgressChange = (event) => {
+    this.video.currentTime = event.target.value
+  }
+
   render() {
     return(
       <VideoPlayerLayout>
@@ -48,17 +53,25 @@ class VideoPlayer extends Component {
         <VideoPlayerControls>
           <PlayPause 
             pause={this.state.pause}
-            handleClick={this.togglePlay} />
-          <Timer 
+            handleClick={this.togglePlay}
+          />
+          <Timer
             duration={this.state.duration}
-            currentTime={this.state.currentTime} />
+            currentTime={this.state.currentTime}
+          />
+          <ProgressBar
+            duration={this.state.duration}
+            value={this.state.currentTime}
+            handleProgressChange={this.handleProgressChange}
+          />
         </VideoPlayerControls>
         <Video
           autoplay={this.props.autoplay}
           pause={this.state.pause}
           handleLoadedMetadata={this.handleLoadedMetadata}
           handleTimeUpdate={this.handleTimeUpdate}
-          src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4" />
+          src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
+        />
       </VideoPlayerLayout>
     )
   }
